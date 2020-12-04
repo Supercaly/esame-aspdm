827846dc3b27fc6adaf94c57a8f21ff7aff14a38
supercaly <djonline96@gmail.com>
2020-12-04 10:05:29 +0100

    Add compact mode to label_widget.dart

--- lib/widgets/label_widget.dart
+++ lib/widgets/label_widget.dart
@@ -1,20 +1,45 @@
 import 'package:aspdm_project/model/label.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
+/// Widget displaying a single [Label] element.
 class LabelWidget extends StatelessWidget {
+  /// [Label] object to display.
   final Label label;
 
-  const LabelWidget(this.label);
+  /// If set to `true` the label will be displayed as
+  /// a rectangular colored box; if set to `false` it
+  /// will display the label's text inside that box.
+  /// Defaults to `false`
+  final bool compact;
+
+  const LabelWidget({
+    Key key,
+    this.label,
+    this.compact = false,
+  })  : assert(label != null),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 42.0,
-      height: 10.0,
+      width: compact ? 42.0 : 82.0,
+      height: compact ? 10.0 : 32.0,
+      //padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
       decoration: BoxDecoration(
         color: label.color,
         borderRadius: BorderRadius.circular(20.0),
       ),
+      child: (!compact)
+          ? Center(
+              child: Text(
+              label.text ?? "",
+              style: Theme.of(context).textTheme.bodyText1.copyWith(
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+            ))
+          : null,
     );
   }
 }
