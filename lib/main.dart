b81e3bc7c154886a49dac2d52fd7d891420161db
supercaly <djonline96@gmail.com>
2020-12-05 11:19:34 +0100

    Fix format and analysis errors for now

--- lib/main.dart
+++ lib/main.dart
@@ -63,4 +63,3 @@ class Root extends StatelessWidget {
     );
   }
 }
-
--- lib/states/task_state.dart
+++ lib/states/task_state.dart
@@ -1,15 +1,13 @@
-import 'package:aspdm_project/model/task.dart';
-
 class TaskState {
-  bool _loading;
-  List<Task> _tasks;
-
-  TaskState() {
-    _loading = false;
-    _tasks = [];
-  }
-
-  Future<Task> getTask(String id) async {}
-
-  Future<List<Task>> getTasks(){}
+  // bool _loading;
+  // List<Task> _tasks;
+  //
+  // TaskState() {
+  //   _loading = false;
+  //   _tasks = [];
+  // }
+  //
+  // Future<Task> getTask(String id) async {}
+  //
+  // Future<List<Task>> getTasks() {}
 }
