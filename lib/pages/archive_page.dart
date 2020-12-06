66e930a62e486516412fefa3244c9fd74d16cd40
supercaly <djonline96@gmail.com>
2020-12-06 11:27:47 +0100

    Create the archive_page.dart widget structure

--- lib/pages/archive_page.dart
+++ lib/pages/archive_page.dart
@@ -1,18 +1,67 @@
-import 'package:aspdm_project/dummy_data.dart';
+import 'package:aspdm_project/bloc/archive_bloc.dart';
+import 'package:aspdm_project/repositories/archive_repository.dart';
 import 'package:aspdm_project/widgets/task_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 class ArchivePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Archived Tasks"),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        itemBuilder: (_, index) => TaskCard(DummyData.archivedTasks[index]),
-        itemCount: DummyData.archivedTasks.length,
+    return BlocProvider<ArchiveBloc>(
+      create: (context) => ArchiveBloc(ArchiveRepository())..fetch(),
+      child: Builder(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: Text("Archived Tasks"),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.filter_list),
+                onPressed: () {
+                  // TODO: Put filter logic here.
+                  print("Filtering...");
+                  context.read<ArchiveBloc>().fetch();
+                },
+              ),
+            ],
+          ),
+          body: BlocConsumer<ArchiveBloc, ArchiveState>(
+            listenWhen: (prev, current) => current.hasError && !prev.hasError,
+            listener: (context, state) =>
+                ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text("Unknown error occurred!")),
+            ),
+            builder: (context, state) => LoadingOverlay(
+              isLoading: state.isLoading,
+              color: Colors.black26,
+              child: RefreshIndicator(
+                onRefresh: () =>
+                    context.read<ArchiveBloc>().fetch(showLoading: false),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    if (!state.isLoading && state.data.isEmpty)
+                      return SingleChildScrollView(
+                        physics: AlwaysScrollableScrollPhysics(),
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                            minHeight: constraints.maxHeight,
+                          ),
+                          child: Center(
+                            child: Text("Nothing to show here!"),
+                          ),
+                        ),
+                      );
+                    return ListView.builder(
+                      itemBuilder: (_, index) => TaskCard(state.data[index]),
+                      itemCount: state.data.length,
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
