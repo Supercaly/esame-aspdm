96bb8e5a0812bcb40d56b2fde145f4672defdb43
supercaly <djonline96@gmail.com>
2020-12-07 14:36:00 +0100

    Add documentation to preference_service.dart

--- lib/services/preference_service.dart
+++ lib/services/preference_service.dart
@@ -2,6 +2,7 @@ import 'package:aspdm_project/model/user.dart';
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Service that manages the [SharedPreferences].
 class PreferenceService {
   SharedPreferences _preferences;
 
@@ -14,12 +15,16 @@ class PreferenceService {
     _preferences = await SharedPreferences.getInstance();
   }
 
+  /// Store the currently logged in [User].
   Future<void> storeSignedInUser(User user) async {
     await _preferences.setString("user_id", user?.id);
     await _preferences.setString("user_name", user?.name);
     await _preferences.setString("user_email", user?.email);
   }
 
+  /// Returns an instance of [User] stored using [storeSignedInUser]
+  /// during the last login.
+  /// If there's no user stored `null` will be returned instead.
   User getLastSignedInUser() {
     final id = _preferences.getString("user_id");
     if (id == null) return null;
