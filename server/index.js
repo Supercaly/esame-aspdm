0c45bfc9764a37e6dec1e652525c46422ec1ebf4
supercaly <djonline96@gmail.com>
2020-12-16 19:39:38 +0100

    Change index.js file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-require('./database');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use("/api/users", require('./routes/users_route'));
+// Connect database
+mongoose.connect("mongodb://localhost:27017/database", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+    .then(() => console.log("Database connected!"))
+    .catch(e => console.error(`Error connecting to database: ${e}`));
+mongoose.connection.on('error', e => console.error(e));
 
-app.listen(3000, () => console.log("Listening server on port 3000!"));
+// Setup Routes
+app.use("/api/", require('./routes/users_route'));
+app.use("/api/list", require('./routes/list_tasks_route'));
+app.use("/api/task", require('./routes/task_route'));
+app.use("/api/comment", require('./routes/comments_route'));
+
+// Listen to server
+app.listen(5000, () => console.log("Listening server on port 3000!"));
---
+++ server/utils.js
@@ -0,0 +1,17 @@
+const ObjectId = require('mongoose').Types.ObjectId;
+
+const idValidator = (v) => ObjectId.isValid(v);
+const idSanitizer = (v) => new ObjectId(v);
+const arrayValidator = (v) => v === undefined || v === null || Array.isArray(v);
+const colorValidator = (v) => v === undefined || v === null || (/^#([0-9a-f]{6})$/i).test(v);
+
+
+exports.idValidator = idValidator;
+exports.idSanitizer = idSanitizer;
+exports.arrayValidator = arrayValidator;
+exports.colorValidator = colorValidator;
+
+exports.populateTask = {
+    path: 'author members comments',
+    populate: {path: 'author like_users dislike_users'},
+}
