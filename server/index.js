ddba8f381a4fe926cf7a3be7d055f94c2cfc9d5e
supercaly <djonline96@gmail.com>
2020-12-23 17:22:05 +0100

    Add cors dependency

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect database
