80adc94225a5322efe0f90a1f61264e952e8a8eb
supercaly <djonline96@gmail.com>
2020-12-25 17:51:11 +0100

    Change settings_page.dart to use responsive

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -4,6 +4,7 @@ import 'package:aspdm_project/services/app_info_service.dart';
 import 'package:aspdm_project/services/log_service.dart';
 import 'package:aspdm_project/services/navigation_service.dart';
 import 'package:aspdm_project/states/auth_state.dart';
+import 'package:aspdm_project/widgets/responsive.dart';
 import 'package:aspdm_project/widgets/settings_widget.dart';
 import 'package:aspdm_project/widgets/user_info_card.dart';
 import 'package:flutter/material.dart';
@@ -14,72 +15,77 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     locator<LogService>().logBuild("SettingsPage");
     final appInfo = locator<AppInfoService>();
-    return ListView(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      children: [
-        UserInfoCard(),
-        SettingsGroup.single(
-          title: "notifications",
-          item: SettingsGroupItem(
-            text: "Show system settings",
-            icon: Icon(Icons.notifications),
-            onTap: null,
-          ),
-        ),
-        SettingsGroup.single(
-          title: "archive",
-          item: SettingsGroupItem(
-            text: "Show archived tasks",
-            icon: Icon(Icons.archive),
-            onTap: () {
-              locator<NavigationService>().navigateTo(Routes.archive);
-            },
-          ),
-        ),
-        SettingsGroup(
-          title: "about",
+    return Center(
+      child: Container(
+        width: Responsive.isLarge(context) ? 500 : double.maxFinite,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
           children: [
-            SettingsGroupItem(
-              text: "About this app",
-              icon: Icon(Icons.bookmark),
+            UserInfoCard(),
+            SettingsGroup.single(
+              title: "notifications",
+              item: SettingsGroupItem(
+                text: "Show system settings",
+                icon: Icon(Icons.notifications),
+                onTap: null,
+              ),
             ),
-            SettingsGroupItem(
-              text: "Open Source",
-              icon: Icon(Icons.adb),
-              onTap: () {
-                showLicensePage(
-                    context: context,
-                    applicationVersion: appInfo.version,
-                    applicationLegalese: "©2020 Lorenzo Calisti");
-              },
+            SettingsGroup.single(
+              title: "archive",
+              item: SettingsGroupItem(
+                text: "Show archived tasks",
+                icon: Icon(Icons.archive),
+                onTap: () {
+                  locator<NavigationService>().navigateTo(Routes.archive);
+                },
+              ),
             ),
-            SettingsGroupItem(
-              text: "Version ${appInfo.version}(${appInfo.buildNumber})",
-              onLongPress: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("This is not an Easter Egg!"),
-                  ),
-                );
-              },
+            SettingsGroup(
+              title: "about",
+              children: [
+                SettingsGroupItem(
+                  text: "About this app",
+                  icon: Icon(Icons.bookmark),
+                ),
+                SettingsGroupItem(
+                  text: "Open Source",
+                  icon: Icon(Icons.adb),
+                  onTap: () {
+                    showLicensePage(
+                        context: context,
+                        applicationVersion: appInfo.version,
+                        applicationLegalese: "©2020 Lorenzo Calisti");
+                  },
+                ),
+                SettingsGroupItem(
+                  text: "Version ${appInfo.version}(${appInfo.buildNumber})",
+                  onLongPress: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("This is not an Easter Egg!"),
+                      ),
+                    );
+                  },
+                ),
+                SettingsGroupItem(
+                  text: "Made with <3 from Italy.",
+                ),
+              ],
             ),
-            SettingsGroupItem(
-              text: "Made with <3 from Italy.",
-            ),
+            SettingsGroup.single(
+              title: "account",
+              item: SettingsGroupItem(
+                text: "Sign Out",
+                icon: Icon(Icons.exit_to_app, color: Colors.red),
+                textColor: Colors.red,
+                onTap: () async {
+                  await context.read<AuthState>().logout();
+                },
+              ),
+            )
           ],
         ),
-        SettingsGroup.single(
-          title: "account",
-          item: SettingsGroupItem(
-            text: "Sign Out",
-            icon: Icon(Icons.exit_to_app, color: Colors.red),
-            textColor: Colors.red,
-            onTap: () async {
-              await context.read<AuthState>().logout();
-            },
-          ),
-        )
-      ],
+      ),
     );
   }
 }
