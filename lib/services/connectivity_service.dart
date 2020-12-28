4d6ada50ded99d804b03ad332c81a8e57e9e6642
supercaly <djonline96@gmail.com>
2020-12-28 17:18:45 +0100

    Add connectivity_service_test.dart

--- lib/services/connectivity_service.dart
+++ lib/services/connectivity_service.dart
@@ -1,22 +1,28 @@
 import 'dart:async';
-
 import 'package:connectivity/connectivity.dart';
+import 'package:flutter/foundation.dart';
 
 class ConnectivityService {
   final Connectivity _connectivity;
 
   ConnectivityService() : _connectivity = Connectivity();
 
+  @visibleForTesting
+  ConnectivityService.private(this._connectivity);
+
   Stream<bool> get onConnectionStateChange =>
       _connectivity.onConnectivityChanged.asBroadcastStream().transform(
-        StreamTransformer.fromHandlers(
-          handleData: (data, sink) {
-            if (data == ConnectivityResult.wifi ||
-                data == ConnectivityResult.mobile)
-              sink.add(true);
-            else
-              sink.add(false);
-          },
-        ),
-      );
+            StreamTransformer.fromHandlers(
+              handleData: (data, sink) {
+                if (data == ConnectivityResult.wifi ||
+                    data == ConnectivityResult.mobile)
+                  sink.add(true);
+                else
+                  sink.add(false);
+              },
+              handleError: (error, stackTrace, sink) {
+                sink.addError(error, stackTrace);
+              },
+            ),
+          );
 }
---
+++ test/services/connectivity_service_test.dart
@@ -0,0 +1,59 @@
+import 'package:aspdm_project/services/connectivity_service.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockConnectivity extends Mock implements Connectivity {}
+
+void main() {
+  group("ConnectivityService Tests", () {
+    ConnectivityService connectivityService;
+    Connectivity mockConnectivity;
+
+    setUpAll(() {
+      mockConnectivity = MockConnectivity();
+      connectivityService = ConnectivityService.private(mockConnectivity);
+    });
+
+    test("onConnectionStateChange emits true if the connection is active", () {
+      when(mockConnectivity.onConnectivityChanged).thenAnswer(
+        (_) => Stream.value(ConnectivityResult.wifi),
+      );
+      expect(connectivityService.onConnectionStateChange, emits(true));
+    });
+
+    test("onConnectionStateChange emits false if the connection is not active",
+        () {
+      when(mockConnectivity.onConnectivityChanged).thenAnswer(
+        (_) => Stream.value(ConnectivityResult.none),
+      );
+      expect(connectivityService.onConnectionStateChange, emits(false));
+    });
+
+    test("onConnectionStateChange emits when connection status changes", () {
+      when(mockConnectivity.onConnectivityChanged).thenAnswer(
+        (_) => Stream.fromIterable([
+          ConnectivityResult.none,
+          ConnectivityResult.mobile,
+          ConnectivityResult.wifi,
+          ConnectivityResult.none,
+        ]),
+      );
+      expect(
+          connectivityService.onConnectionStateChange,
+          emitsInOrder([
+            false,
+            true,
+            true,
+            false,
+          ]));
+    });
+
+    test("onConnectionStateChange emits error if there's an error", () {
+      when(mockConnectivity.onConnectivityChanged).thenAnswer(
+        (_) => Stream.error("Error"),
+      );
+      expect(connectivityService.onConnectionStateChange, emitsError("Error"));
+    });
+  });
+}
