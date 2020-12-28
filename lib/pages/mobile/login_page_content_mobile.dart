124821f0293a33b4b1ee3ff30d5e7d4d0b4cd0b9
supercaly <djonline96@gmail.com>
2020-12-28 18:42:15 +0100

    Fix login_page_content_mobile.dart rotation issue that caused overflow of small devices

--- lib/pages/mobile/login_page_content_mobile.dart
+++ lib/pages/mobile/login_page_content_mobile.dart
@@ -5,17 +5,27 @@ class LoginPageContentMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Card(
-        margin: const EdgeInsets.all(25.0),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Placeholder(fallbackHeight: 128.0),
-              SizedBox(height: 8.0),
-              LoginForm(),
-            ],
+      child: SingleChildScrollView(
+        child: LayoutBuilder(
+          builder: (context, constraints) => ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: constraints.maxWidth,
+              maxHeight: constraints.maxHeight,
+            ),
+            child: Card(
+              margin: const EdgeInsets.all(25.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Placeholder(fallbackHeight: 128.0),
+                    SizedBox(height: 8.0),
+                    LoginForm(),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
