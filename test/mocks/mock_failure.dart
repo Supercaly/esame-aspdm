00dac60b403b9c89f2d7dbf044738360858c72c2
supercaly <djonline96@gmail.com>
2021-01-10 19:15:45 +0100

    Fix mock_failure.dart

--- test/mocks/mock_failure.dart
+++ test/mocks/mock_failure.dart
@@ -1,6 +1,3 @@
 import 'package:aspdm_project/domain/failures/failures.dart';
 
-class MockFailure extends Failure {
-  @override
-  List<Object> get props => [];
-}
+class MockFailure extends Failure {}
