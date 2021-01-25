779e66d6651d14c6164643b23a29ba70698ce963
supercaly <djonline96@gmail.com>
2021-01-25 17:53:41 +0100

    Add TODO(#56)

--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -6,4 +6,25 @@ class Routes {
   static final String login = "/login";
   static final String archive = "/archive";
   static final String task = "/task";
+
+  // TODO(#56): Extract the route arguments in onGenerateRoute
+  // Add `onGenerateRoute` static method to `Routes` that creates each routes and extract the correct arguments from it automatically.
+  // ```dart
+  // static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  //     final args = settings.arguments;
+  //     switch (settings.name) {
+  //       case Router.splashPage:
+  //         return MaterialPageRoute<dynamic>(
+  //           builder: (_) => SplashPage(),
+  //           settings: settings,
+  //         );
+  //       case Router.noteFormPage:
+  //         // extract the arguments
+  //         return MaterialPageRoute<dynamic>(
+  //           builder: (_) => NoteFormPage(editedNote: typedArgs.editedNote),
+  //           settings: settings,
+  //         );
+  //     }
+  //   }
+  // ```
 }
