cde5c0388d3ac4c4abc8c04177833d68d6d64023
supercaly <djonline96@gmail.com>
2021-01-29 18:20:18 +0100

    Add label_values.dart

---
+++ lib/domain/values/label_values.dart
@@ -0,0 +1,33 @@
+import 'package:aspdm_project/core/either.dart';
+import 'package:aspdm_project/core/value_object.dart';
+
+/// Class representing a valid label name.
+class LabelName extends ValueObject {
+  static const int maxLength = 30;
+
+  @override
+  final Either<ValueFailure, dynamic> value;
+
+  const LabelName._(this.value);
+
+  /// Creates a [LabelName] from an input [String] that has
+  /// at most [maxLength] characters.
+  /// The input can't be null, empty or longer than [maxLength].
+  factory LabelName(String input) {
+    if (input == null || input.isEmpty)
+      return LabelName._(Either.left(ValueFailure.empty(input)));
+    if (input.length > maxLength)
+      return LabelName._(Either.left(ValueFailure.tooLong(input)));
+    return LabelName._(Either.right(input));
+  }
+
+  /// Creates a [LabelName] with empty content.
+  /// NOTE: The new [LabelName] will have value that is the left
+  /// side of the Either (it's invalid).
+  factory LabelName.empty() =>
+      LabelName._(Either.left(ValueFailure.empty(null)));
+
+  @override
+  String toString() =>
+      "LabelName(${value.fold((left) => left, (right) => right)})";
+}
