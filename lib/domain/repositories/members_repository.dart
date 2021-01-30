fb41ee30d93171fb9c5d4cdf2ae32e713ccf1042
supercaly <djonline96@gmail.com>
2021-01-30 16:27:39 +0100

    Add IList to members_repository.dart

--- lib/domain/repositories/members_repository.dart
+++ lib/domain/repositories/members_repository.dart
@@ -1,7 +1,8 @@
 import 'package:aspdm_project/core/either.dart';
+import 'package:aspdm_project/core/ilist.dart';
 import 'package:aspdm_project/domain/entities/user.dart';
 import 'package:aspdm_project/domain/failures/failures.dart';
 
 abstract class MembersRepository {
-  Future<Either<Failure, List<User>>> getUsers();
+  Future<Either<Failure, IList<User>>> getUsers();
 }
