57656c8043b427fa34c4803a4ee078eb0a08cc4e
supercaly <djonline96@gmail.com>
2021-02-01 21:16:21 +0100

    Add test method to send notifications

--- server/index.js
+++ server/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const admin = require('firebase-admin');
 require('dotenv').config();
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://aspdm-project.firebaseio.com',
+});
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Connect database
-const dbUri = `mongodb+srv://${process.env.USER}:${process.env.PWD}@${process.env.HOST}/${process.env.DB}?retryWrites=true&w=majority`
+//const dbUri = `mongodb+srv://${process.env.USER}:${process.env.PWD}@${process.env.HOST}/${process.env.DB}?retryWrites=true&w=majority`
+const dbUri = "mongodb://localhost:27017/database";
 mongoose.connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -25,6 +32,14 @@ app.use("/api/list", require('./routes/list_tasks_route'));
 app.use("/api/task", require('./routes/task_route'));
 app.use("/api/comment", require('./routes/comments_route'));
 app.use("/api/labels", require('./routes/label_route'));
-
+app.get("/not", async  (req, res) => {
+    console.log("ciao");
+    await admin.messaging().send({
+        topic: "newtask",
+        notification: {title: "Ciao", body: "Test Test"},
+        data: {task_id: "prova"}
+    });
+    res.sendStatus(200);
+})
 // Listen to server
 app.listen(process.env.PORT, () => console.log(`Listening server on port ${process.env.PORT}!`));
