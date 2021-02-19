7705678814aec5157e8d55ffcdafcbe6078a4eb5
Lorenzo <djonline96@gmail.com>
2021-02-19 17:21:32 +0100

    Use sliding bottom sheet in label_picker_sheet.dart

--- lib/presentation/pages/task_form/widgets/label_picker_sheet.dart
+++ lib/presentation/pages/task_form/widgets/label_picker_sheet.dart
@@ -1,7 +1,9 @@
+import 'package:sliding_sheet/sliding_sheet.dart';
 import 'package:tasky/application/bloc/labels_bloc.dart';
 import 'package:tasky/core/ilist.dart';
 import 'package:tasky/domain/entities/label.dart';
 import 'package:tasky/domain/repositories/label_repository.dart';
+
 import 'package:tasky/presentation/pages/task_form/widgets/label_picker_item_widget.dart';
 import 'package:tasky/presentation/pages/task_form/widgets/sheet_notch.dart';
 import 'package:flutter/material.dart';
@@ -11,103 +13,108 @@ import 'package:easy_localization/easy_localization.dart';
 import '../../../../locator.dart';
 
 /// Display a bottom sheet that picks the labels.
-/// Passing an existing [List] of [labels] to mark them as already selected.
-/// Returns a [List] of all the selected [Label]s.
+/// Passing an existing [IList] of [labels] to mark them as already selected.
+/// Returns a [IList] of all the selected [Label]s.
 Future<IList<Label>> showLabelPickerSheet(
   BuildContext context,
   IList<Label> labels,
 ) {
-  return showModalBottomSheet(
-    context: context,
-    builder: (context) => LabelPickerSheet(labels: labels),
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+  return showSlidingBottomSheet(
+    context,
+    resizeToAvoidBottomInset: false,
+    parentBuilder: (context, sheet) => BlocProvider<LabelsBloc>(
+      create: (context) => LabelsBloc(
+        initialValue: labels,
+        repository: locator<LabelRepository>(),
+      )..fetch(),
+      child: sheet,
     ),
-  );
-}
-
-/// Widget that display a bottom sheet that picks labels
-class LabelPickerSheet extends StatelessWidget {
-  final IList<Label> labels;
-
-  LabelPickerSheet({Key key, this.labels}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 16.0,
-        left: 24.0,
-        bottom: 0.0,
-        right: 24.0,
+    builder: (context) => SlidingSheetDialog(
+      cornerRadius: 16.0,
+      avoidStatusBar: true,
+      cornerRadiusOnFullscreen: 0.0,
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+      snapSpec: SnapSpec(
+        snap: true,
+        initialSnap: 0.6,
+        snappings: const [0.6, 1],
+        positioning: SnapPositioning.relativeToAvailableSpace,
       ),
-      child: BlocProvider<LabelsBloc>(
-        create: (context) => LabelsBloc(
-          initialValue: labels,
-          repository: locator<LabelRepository>(),
-        )..fetch(),
-        child: BlocBuilder<LabelsBloc, LabelsState>(
-          builder: (context, state) {
-            Widget contentWidget;
-
-            if (state.isLoading)
-              contentWidget = Center(child: CircularProgressIndicator());
-            else if (state.hasError)
-              contentWidget =
-                  Center(child: Text('no_label_to_display_msg').tr());
-            else
-              contentWidget = CustomScrollView(
-                slivers: [
-                  SliverList(
-                    delegate: SliverChildListDelegate.fixed(
-                      state.labels
-                          .map(
-                            (e) => LabelPickerItemWidget(
-                              label: e,
-                              selected: state.selected.contains(e),
-                              onSelected: (selected) {
-                                if (selected)
-                                  context.read<LabelsBloc>().selectLabel(e);
-                                else
-                                  context.read<LabelsBloc>().deselectLabel(e);
-                              },
-                            ),
-                          )
-                          .asList(),
-                    ),
-                  ),
-                ],
-              );
-
-            return Column(
+      headerBuilder: (context, state) => Padding(
+        padding: const EdgeInsets.only(
+          top: 16,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SheetNotch(),
+            SizedBox(height: 8.0),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                SheetNotch(),
-                SizedBox(height: 8.0),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'select_labels_title',
-                      style: Theme.of(context).textTheme.headline6,
-                    ).tr(),
-                    TextButton(
-                      onPressed: () {
-                        locator<NavigationService>().pop(
-                          result: state.selected,
-                        );
-                      },
-                      child: Text('save_btn').tr(),
-                    ),
-                  ],
+                Text(
+                  'select_labels_title',
+                  style: Theme.of(context).textTheme.headline6,
+                ).tr(),
+                TextButton(
+                  onPressed: () {
+                    locator<NavigationService>().pop(
+                      result: context.read<LabelsBloc>().state.selected,
+                    );
+                  },
+                  child: Text('save_btn').tr(),
                 ),
-                SizedBox(height: 16.0),
-                Expanded(child: contentWidget),
               ],
-            );
-          },
+            ),
+          ],
         ),
       ),
-    );
-  }
+      builder: (context, sheetState) {
+        return BlocBuilder<LabelsBloc, LabelsState>(
+          builder: (context, state) {
+            if (state.isLoading)
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                child: Center(child: CircularProgressIndicator()),
+              );
+            else if (state.hasError || state.labels.isEmpty)
+              return Material(
+                color: Theme.of(context).cardColor,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 250.0,
+                    minHeight: 100.0,
+                  ),
+                  child: Center(
+                    child: Text('no_label_to_display_msg').tr(),
+                  ),
+                ),
+              );
+            else
+              return ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) => LabelPickerItemWidget(
+                  label: state.labels[index],
+                  selected: state.selected.contains(state.labels[index]),
+                  onSelected: (selected) {
+                    if (selected)
+                      context
+                          .read<LabelsBloc>()
+                          .selectLabel(state.labels[index]);
+                    else
+                      context
+                          .read<LabelsBloc>()
+                          .deselectLabel(state.labels[index]);
+                  },
+                ),
+                itemCount: state.labels.length,
+              );
+          },
+        );
+      },
+    ),
+  );
 }
